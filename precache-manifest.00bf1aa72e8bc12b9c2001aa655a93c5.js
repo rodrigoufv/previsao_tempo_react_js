f3908a5926e1bf640bdbbcd3d1ee97d1ca10594e
Rodrigo Sidney <rodrigosidney77@gmail.com>
2022-06-20 16:11:06 -0300

    Updates

---
+++ precache-manifest.00bf1aa72e8bc12b9c2001aa655a93c5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "abd8d638376391308c82",
+    "url": "/previsao_tempo_react_js/static/css/main.ab92e1c5.chunk.css"
+  },
+  {
+    "revision": "abd8d638376391308c82",
+    "url": "/previsao_tempo_react_js/static/js/main.abd8d638.chunk.js"
+  },
+  {
+    "revision": "55feba063e7c3ffa282a",
+    "url": "/previsao_tempo_react_js/static/js/1.55feba06.chunk.js"
+  },
+  {
+    "revision": "28d5962f77355f761445",
+    "url": "/previsao_tempo_react_js/static/js/runtime~main.28d5962f.js"
+  },
+  {
+    "revision": "551232eaa35bf73ea64ef48d3f5b0327",
+    "url": "/previsao_tempo_react_js/static/media/background.551232ea.jpg"
+  },
+  {
+    "revision": "c328f9c171abd1b785bff9b332774560",
+    "url": "/previsao_tempo_react_js/index.html"
+  }
+];
